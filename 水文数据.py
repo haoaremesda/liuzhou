3e56a36946023ffe59e5c4c2b0821614a97746b6
haoaremesda <1771380243@qq.com>
2023-10-21 17:07:35 +0800

    update spider

--- 水文数据.py
+++ 水文数据.py
@@ -1,9 +1,12 @@
 import json
+import os
+import re
 import time
 
 import requests
 from datetime import datetime, timedelta
 from requests_toolbelt.multipart.encoder import MultipartEncoder
+from apscheduler.schedulers.blocking import BlockingScheduler
 import pandas as pd
 
 req_session = requests.session()
@@ -75,27 +78,43 @@ def send_sczwfw_data(url, app_id, interface_id, currentPage, pageSize, fieldValu
 
 
 def get_sczwfw_data():
-    global sczwfw_data, sczwfw_names, fields
+    global sczwfw_names, fields, file_path, IsRun, sczwfw_summary_file_path, sczwfw_datas_file_path
+    current_date = datetime.now()
+    yesterday = (current_date - timedelta(days=1)).strftime("%Y-%m-%d") + " 00:00:00"
     createsign_do_url = "https://tftb.sczwfw.gov.cn:8085/jmas-api-gateway-server/createsign.do"
     gateway_do_url = "https://tftb.sczwfw.gov.cn:8085/jmas-api-gateway-server/gateway.do"
+    station_datas = []
     for i in sczwfw_names:
-        station_datas = []
         total = 0
         page, page_size = 1, 60
         if i in ['雅江（三）', '桐子林（二）']:
-            columns = {"zhanming": "站名", "zm": "站码", "sj": "时间", "sw": "水位（m）", "hlmc": "河流名称", "xzqh": "行政区划", "lymc": "流域名称", "ll": "流量(m³/s)"}
+            columns = {"zhanming": "站名", "zm": "站码", "hlmc": "河流名称", "xzqh": "行政区划", "lymc": "流域名称",
+                       "sw": "水位(m)", "rkll": "入库流量(m³/s)", "ll": "出库流量(m³/s)", "xsl": "蓄水量(百万m²)",
+                       "sj": "时间"}
             app_id, interface_id = "sltqszdhdsssqxxpc", "sltqszdhdsssqxx"
         else:
-            columns = {"zhanming": "站名", "zm": "站码", "hlmc": "河流名称", "xzqh": "行政区划", "lymc": "流域名称", "ksw": "库水位（m）", "rkll": "入库流量（m3/s）", "ckll": "出库流量（m3/s）", "xsl": "蓄水量 (百万m² )", "sj": "时间"}
+            columns = {"zhanming": "站名", "zm": "站码", "hlmc": "河流名称", "xzqh": "行政区划", "lymc": "流域名称", "ksw": "水位(m)", "rkll": "入库流量(m³/s)", "ckll": "出库流量(m³/s)", "xsl": "蓄水量(百万m²)", "sj": "时间"}
             app_id, interface_id = "sltqszdsksssqxxpc", "sltqszdsksssqxx"
         while True:
+            # if page > 30:
+            #     break
             if total != 0 and (page_size * page - total) >= page_size:
                 break
             timestamp = str(int(time.time() * 1e3))
-            sign_code, sign_json_data = send_sczwfw_data(createsign_do_url, app_id, interface_id, page, page_size, i, timestamp)
+            try:
+                sign_code, sign_json_data = send_sczwfw_data(createsign_do_url, app_id, interface_id, page, page_size, i, timestamp)
+            except Exception as e:
+                print(e)
+                time.sleep(1)
+                continue
             if sign_code == 200 and sign_json_data["success"]:
                 sign = sign_json_data["data"]["sign"]
-                status_code, json_data = send_sczwfw_data(gateway_do_url, app_id, interface_id, page, page_size, i, timestamp, sign)
+                try:
+                    status_code, json_data = send_sczwfw_data(gateway_do_url, app_id, interface_id, page, page_size, i, timestamp, sign)
+                except Exception as e:
+                    print(e)
+                    time.sleep(1)
+                    continue
                 if status_code == 200:
                     d = json.loads(json_data["data"])
                     if d["status"] == 0:
@@ -103,7 +122,11 @@ def get_sczwfw_data():
                             if total == 0:
                                 total = d['result']['data']['total']
                             for item in d['result']['data']['list']:
+                                if IsRun and item["sj"] < yesterday:
+                                    total = page_size
+                                    break
                                 station_datas.append([item[s] if s in item else '' for s in columns])
+                            print(i, page)
                             page += 1
                         else:
                             print(d)
@@ -114,23 +137,74 @@ def get_sczwfw_data():
             else:
                 print(sign_code, sign_json_data)
                 time.sleep(10)
-        station_datas.reverse()
-        df = pd.DataFrame(station_datas, columns=list(columns.values()))
-        # 将DataFrame保存为Excel文件
-        df.to_excel(f'{i}.xlsx', index=False)
+            # time.sleep(1)
+    if not station_datas:
+        return False
+    station_datas.reverse()
+    df = pd.DataFrame(station_datas, columns=['站名', '站码', '河流名称', '行政区划', '流域名称', '水位(m)', '入库流量(m³/s)', '出库流量(m³/s)', '蓄水量(百万m²)', '时间'])
+    station_datas = []
+    ks = ['水位(m)', '出库流量(m³/s)', "入库流量(m³/s)", "蓄水量(百万m²)"]
+    means = {k: 'mean' for k in ks}
+    for k in ks:
+        df[k] = pd.to_numeric(df[k], errors='coerce')
+    df['时间'] = pd.to_datetime(df['时间'])
+    df['时间'] = df['时间'].dt.date
+    df = df[df['时间'] != current_date.date()]
+    result = df.groupby(['站名', '时间']).agg(means).reset_index()
+    # 如果文件不存在，创建新的Excel文件并写入数据
+    for k in ks:
+        pivot_df = result[["时间", "站名", k]].pivot(index='时间', columns='站名', values=k)
+        pivot_df.reset_index(inplace=True)
+        k = re.sub(r'\(.*\)', '', k)
+        path = f'{sczwfw_summary_file_path}/任务3 {k}.csv'
+        if os.path.exists(path) and IsRun:
+            pivot_df.to_csv(path, mode='a', header=False, index=False)
+        else:
+            pivot_df.to_csv(path, index=False)
+    # 将DataFrame保存为Excel文件
+    if os.path.exists(sczwfw_datas_file_path) and IsRun:
+        df.to_csv(sczwfw_datas_file_path, mode='a', header=False, index=False)
+    else:
+        df.to_csv(sczwfw_datas_file_path, index=False)
 
 
-
-
-
+def run():
+    global IsRun
+    get_ynswj_data()
+    get_schwr_data()
+    for c in ynswj_data.items():
+        columns = ["时间"] + list(c[1].keys())
+        values = [[datetime.now().strftime("%Y-%m-%d")] + list(c[1].values())]
+        df = pd.DataFrame(values, columns=columns)
+        path = f'{sczwfw_summary_file_path}/任务1&2 {c[0]}.csv'
+        if os.path.exists(path) and IsRun:
+            df.to_csv(path, mode='a', header=False, index=False)
+        else:
+            df.to_csv(path, index=False)
+    print("任务1&2执行完成")
+    get_sczwfw_data()
+    print("任务3执行完成")
+    IsRun = True
+    print("今日已运行")
 
 if __name__ == '__main__':
+    IsRun = False
+    sczwfw_summary_file_path = './汇总数据'
+    if not os.path.exists(sczwfw_summary_file_path):
+        os.makedirs(sczwfw_summary_file_path)
+    sczwfw_datas_file_path = f'{sczwfw_summary_file_path}/任务三 备份数据.csv'
     fields = ["水位", "出库流量", "入库流量", "蓄水池"]
-    ynswj_names = ['黄登', '大华桥', '苗尾', '功果桥', '小湾', '乌东德', '糯扎渡', '景洪', '溪洛渡水库', '向家坝水库']
+    ynswj_names = ['黄登', '大华桥', '苗尾', '功果桥', '小湾', '乌东德水库', '糯扎渡', '景洪', '溪洛渡水库', '向家坝水库']
     ynswj_data = {i: {s: "-" for s in ynswj_names} for i in fields}
     sczwfw_names = ['雅江（三）', '桐子林（二）', '二滩', '锦屏', '官地']
-    sczwfw_data = {}
-    # get_ynswj_data()
-    # get_schwr_data()
-    get_sczwfw_data()
+    # 创建调度器
+    scheduler = BlockingScheduler()
+    # 添加定时任务，每天下午两点执行一次
+    scheduler.add_job(run, 'cron', hour=16, minute=53)
+    try:
+        # 开始调度器
+        scheduler.start()
+    except (KeyboardInterrupt, SystemExit):
+        # 如果需要手动停止程序，按下Ctrl+C
+        pass
     print(ynswj_data)
